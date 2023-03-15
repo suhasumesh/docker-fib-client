c875cfa4f199d0e0262351656b30a5d151593eea
suhasumesh <suhasumesh2002@gmail.com>
2023-03-15 11:12:12 +0530

    Added the Dockerfile

---
+++ src/otherpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const otherpage = () => {
+  return (
+    <div>I am in otherpage
+        <Link to="/">Go back Home</Link>
+    </div>
+  )
+}
+
+export default otherpage
